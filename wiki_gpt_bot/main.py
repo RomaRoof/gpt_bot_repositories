4ec1c266f89252e041afebbb425fa26ec1f36e57
RomaRoof <41184185+RomaRoof@users.noreply.github.com>
2024-08-08 12:59:35 +0500

    удален проверочный код

--- wiki_gpt_bot/main.py
+++ wiki_gpt_bot/main.py
@@ -1,16 +1,30 @@
-import os
-import openai
-from aiogram import Bot, Dispatcher
+import asyncio
 import logging
+import os
+
+from aiogram import Bot, Dispatcher
+from dotenv import load_dotenv
+
+from hendlers import register_handlers
 
 logging.basicConfig(level=logging.INFO)
 
 GPT_MODEL = "gpt-3.5-turbo"
 
+load_dotenv('config.env')
+
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
+register_handlers(dp)
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
 if __name__ == '__main__':
-    dp.start_polling(bot)
+    asyncio.run(main())
